13943b937e0ed416dceaaf61e9d85a1b385a8278
AtheDev <athe.dev@gmail.com>
2021-10-07 19:27:47 +0200

    Ajout de log de debug

--- Module_07/ex00/main.cpp
+++ Module_07/ex00/main.cpp
@@ -33,7 +33,10 @@ int main()
     int a = 2;
     int b = 3;
 
+    std::cout << "==> Before SWAP :" << std::endl;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
     ::swap( a, b );
+    std::cout << "==> After SWAP :" << std::endl;
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
@@ -42,7 +45,10 @@ int main()
     std::string c = "chaine1";
     std::string d = "chaine2";
 
+    std::cout << "==> Before SWAP :" << std::endl;
+    std::cout << "c = " << c << ", d = " << d << std::endl;
     ::swap(c, d);
+    std::cout << "==> After SWAP :" << std::endl;
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
@@ -51,7 +57,10 @@ int main()
     char e = 'a';
     char f = 'z';
 
+    std::cout << "==> Before SWAP :" << std::endl;
+    std::cout << "e = " << e << ", f = " << f << std::endl;
     ::swap( e, f );
+    std::cout << "==> After SWAP :" << std::endl;
     std::cout << "e = " << e << ", f = " << f << std::endl;
     std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
     std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
@@ -60,7 +69,10 @@ int main()
     float g = 1.66f;
     float h = 1.67f;
 
+    std::cout << "==> Before SWAP :" << std::endl;
+    std::cout << "g = " << g << ", h = " << h << std::endl;
     ::swap(g, h);
+    std::cout << "==> After SWAP :" << std::endl;
     std::cout << "g = " << g << ", h = " << h << std::endl;
     std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
     std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
@@ -69,7 +81,10 @@ int main()
     Test i(10);
     Test j(25);
 
+    std::cout << "==> Before SWAP :" << std::endl;
+    std::cout << "i = " << i.getNum() << ", j = " << j.getNum() << std::endl;
     ::swap(i, j);
+    std::cout << "==> After SWAP :" << std::endl;
     std::cout << "i = " << i.getNum() << ", j = " << j.getNum() << std::endl;
     std::cout << "min( i, j ) = " << ::min( i.getNum(), j.getNum()) << std::endl;
     std::cout << "max( i, j ) = " << ::max( i.getNum(), j.getNum()) << std::endl;
